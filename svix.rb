1b66e1beee28cf823cbd9fb5c83e4a27ee024231
svix-ci <support@svix.com>
2023-09-29 16:24:16 -0400

    Brew formula update for svix version v0.21.1

--- svix.rb
+++ svix.rb
@@ -5,20 +5,20 @@
 class Svix < Formula
   desc "Svix CLI utility"
   homepage "https://www.svix.com"
-  version "0.19.0"
+  version "0.21.1"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/svix/svix-cli/releases/download/v0.19.0/svix_0.19.0_Darwin_arm64.tar.gz"
-      sha256 "64a9334b284dd41f288769b246bdf499da8700fb0abd77e6e5233d145ff383f4"
+    if Hardware::CPU.intel?
+      url "https://github.com/svix/svix-cli/releases/download/v0.21.1/svix_Darwin_x86_64.tar.gz"
+      sha256 "f3c74e7ef93d728cf5a064c9967eb28e90c644d7f2677384a4cff5620bc983db"
 
       def install
         bin.install "svix"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/svix/svix-cli/releases/download/v0.19.0/svix_0.19.0_Darwin_x86_64.tar.gz"
-      sha256 "ae89b8368bc4343f6dca409ce2df65a054a8a1a21e33c8579e37460c56f8ab02"
+    if Hardware::CPU.arm?
+      url "https://github.com/svix/svix-cli/releases/download/v0.21.1/svix_Darwin_arm64.tar.gz"
+      sha256 "4600200f47e8bceeb8ae16f64780b427947c2a576c0e10721a4f313aa4cc8086"
 
       def install
         bin.install "svix"
@@ -27,17 +27,17 @@ class Svix < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/svix/svix-cli/releases/download/v0.19.0/svix_0.19.0_Linux_x86_64.tar.gz"
-      sha256 "ddf14e0b9d711c91a6e6dedd180f33e5125c8620e7182283d323769ae1cac3aa"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/svix/svix-cli/releases/download/v0.21.1/svix_Linux_arm64.tar.gz"
+      sha256 "f9eae1e5b805206ada186218be8bb06f601907372f7d72686792417eacda3196"
 
       def install
         bin.install "svix"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/svix/svix-cli/releases/download/v0.19.0/svix_0.19.0_Linux_arm64.tar.gz"
-      sha256 "82c6f23d3040db67872615ec98b9b9cc033600bd6c4dc0c83b6924f914230da0"
+    if Hardware::CPU.intel?
+      url "https://github.com/svix/svix-cli/releases/download/v0.21.1/svix_Linux_x86_64.tar.gz"
+      sha256 "77e66dcce6fb5621d2698fa99370fa34e90a96b5890622acde2a828ed41a3807"
 
       def install
         bin.install "svix"
